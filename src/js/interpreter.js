68909158e97646c76e39d06de671dfb837354fe5
Kabir Goel <kabirgoel.kg@gmail.com>
2017-08-19 11:01:12 +0530

    Fix style issues

--- src/js/interpreter.js
+++ src/js/interpreter.js
@@ -5,14 +5,14 @@
 
 // Rather than hardcoding the value of each token, use constants
 // This allows me to change the syntax of the language easily
-const TOKEN_INCR       = '+';
-const TOKEN_DECR       = '-';
-const TOKEN_LEFT       = '<';
-const TOKEN_RIGHT      = '>';
+const TOKEN_INCR = '+';
+const TOKEN_DECR = '-';
+const TOKEN_LEFT = '<';
+const TOKEN_RIGHT = '>';
 const TOKEN_LOOP_START = '[';
-const TOKEN_LOOP_STOP  = ']';
-const TOKEN_INPUT      = ',';
-const TOKEN_PRINT      = '.';
+const TOKEN_LOOP_STOP = ']';
+const TOKEN_INPUT = ',';
+const TOKEN_PRINT = '.';
 
 /*
  * Parse a blob of Brainfuck code
@@ -39,7 +39,7 @@ function parse(blob) {
           } else if (blob[i] == TOKEN_LOOP_STOP) {
             balance--;
             if (balance > 0) {
-              innerContent += blob[i]
+              innerContent += blob[i];
             }
           } else {
             innerContent += blob[i];
@@ -47,12 +47,12 @@ function parse(blob) {
           i++;
         } while (balance != 0 && i < blob.length);
         if (balance != 0) {
-          return { 'error': 'unterminated loop' }
+          return {'error': 'unterminated loop'};
         }
         // recursive goodness -- the base case is code without loops
         let innerParsed = parse(innerContent);
         if (innerParsed.error != '') {
-          return innerParsed
+          return innerParsed;
         }
         res.push(innerParsed.res);
     } else {
@@ -60,7 +60,7 @@ function parse(blob) {
     }
   }
 
-  return { 'res': res, 'error': '' }
+  return {'res': res, 'error': ''};
 }
 
 /*
@@ -73,13 +73,13 @@ function parse(blob) {
  */
 function eval(blob) {
   // generate 30000 cells
-  let mem = Array.apply(null, Array(30000)).map(Number.prototype.valueOf, 0);
+  let mem = Array(...Array(30000)).map(Number.prototype.valueOf, 0);
   let datPtr = 0;
   let programOutput = [];
 
-  // used for checking the balance of loop start and end delimiters ('[' and ']')
+  // used for checking the balance of loop start and end delimiters
   // has to be declared here because of weird switch statement semantics
-  var balance;
+  let balance;
   for (let i = 0; i < blob.length; i++) {
     switch (blob[i]) {
       case TOKEN_INCR:
@@ -101,7 +101,7 @@ function eval(blob) {
         programOutput.push(String.fromCharCode(mem[datPtr]));
         break;
       case TOKEN_LOOP_START:
-        balance = 0
+        balance = 0;
         // if the current cell is 0, skip to the end of the loop
         // i.e. don't loop
         if (mem[datPtr] == 0) {
@@ -116,12 +116,12 @@ function eval(blob) {
             i++;
           } while (balance != 0 && i < blob.length);
           if (balance != 0) {
-            return { 'error': 'unterminated loop' }
+            return {'error': 'unterminated loop'};
           }
         }
         break;
       case TOKEN_LOOP_STOP:
-        balance = 0
+        balance = 0;
         if (mem[datPtr] > 0) {
           do {
             if (blob[i] == TOKEN_LOOP_START) {
@@ -132,16 +132,16 @@ function eval(blob) {
             i--;
           } while (balance != 0 && i < blob.length);
           if (balance != 0) {
-            return { 'error': 'unterminated loop' }
+            return {'error': 'unterminated loop'};
           }
         }
         break;
     }
   }
-  return { 'ptrPos': datPtr, 'stdout': programOutput, 'mem': mem, 'error': '' }
+  return {'ptrPos': datPtr, 'stdout': programOutput, 'mem': mem, 'error': ''};
 }
 
 module.exports = {
   'parse': parse,
-  'eval': eval
-}
+  'eval': eval,
+};
