9099318d4ed8af70eae34caf355257e2757814a5
Kabir Goel <kabirgoel.kg@gmail.com>
2017-08-19 11:42:23 +0530

    Port to other dude's code

--- src/html/index.js
+++ src/html/index.js
@@ -1,6 +1,15 @@
 toplist = ["Home", "Learn" ,"Documentation", "Updates", "Interpreters"]
 $(".bar").append('<div class="topbar"><div class="rightlist"></div></div>')
 for (var i = 0; i < toplist.length; i++) {
-	$(".rightlist").append('<div class="list" id="'+ toplist[i] +'">'+ toplist[i] +'</div>')
-	
+  $(".rightlist").append('<div class="list" id="'+ toplist[i] +'">'+ toplist[i] +'</div>')
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  let output = document.getElementById('output');
+  let evalForm = document.getElementById('eval-form');
+  let input = document.getElementById('code-input');
+  evalForm.onsubmit = function(e) {
+    e.preventDefault();
+    output.innerHTML = eval(input.value).stdout.join('');
+  };
+});
