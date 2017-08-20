cb47a057c185fbfd61cb74d00f31731faa4d55a3
maruthgoyal <maruthgoyal@gmail.com>
2017-08-20 23:22:26 +0530

    Revert "COЯE: Language Extensions "

--- src/cpp/eval.hpp
+++ src/cpp/eval.hpp
@@ -1,18 +1,10 @@
 #ifndef EVAL_INCLUDE
 #define EVAL_INCLUDE
 
-#include "parse.hpp"
-#include "parse.cpp"
-
-
-int command(node * , char , int, int, int);
-void loop(node *st, int ptr, int mxptr, int cur_size);
-void gr_table(int mxptr , int cur_size);
-int command(node *cur, char c, int ptr, int mxptr, int cur_size);
-void evaluate(node *cmd);
-int pow(int base, int exp);
-int fact(int n);
-int sqrt(int n);
-void com_sp(node *cur , int ptr , int mxptr , int cur_size);
+#include <iostream>
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+#include<string>
 
 #endif
